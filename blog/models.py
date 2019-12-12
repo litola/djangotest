cfe4196d8a2f00278138ff8a9edb0f8290190733
litola <ing.lara@live.com>
2019-12-12 12:37:37 -0600

    blog and polish

--- blog/models.py
+++ blog/models.py
@@ -6,5 +6,15 @@ class Blog(models.Model):
   pub_date = models.DateField(auto_now_add = True)
   image = models.ImageField(upload_to='images/')
 
+  def __str__(self):
+    return self.title
+    
+  def summary(self):
+    return self.body[:100]
+
+  def pub_date_pretty(self):
+    return self.pub_date.strftime('%b %e %Y')
+
+
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+def allblogs(request):
+  posts = Blog.objects
+  return render(request, 'blog/allblogs.html', {'posts':posts})
+
+def post(request, blog_id):
+  post = get_object_or_404(Blog, pk=blog_id)
+  return render(request, 'blog/post.html', {'post':post})
+
 
 # Create your views here.
